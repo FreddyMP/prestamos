ae3aa96f91098d44433a7e28f066945a48b9b26d
Freddy Pereyra <freddypimpns@gmail.com>
2024-02-01 11:02:05 -0400

    Modificar ip en conexion mysql

--- src/config/config.py
+++ src/config/config.py
@@ -2,7 +2,7 @@ import pymysql
 import hashlib
 
 def exist(nombre):
-    db = pymysql.connect(host= 'localhost', port= 3306, user= 'root', password='')
+    db = pymysql.connect(host= 'localhost', port= 3306, user= 'fpereyra', password='15951236487')
 
     cursor =  db.cursor(pymysql.cursors.DictCursor)
 
